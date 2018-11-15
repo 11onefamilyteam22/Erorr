8fe311b48e15fb557e236ae32defc13ea1d6aa5d
11onefamilyteam22 <45063802+11onefamilyteam22@users.noreply.github.com>
2018-11-15 20:00:44 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,27 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "*"
+
+console.log("Welcome Again !");
+ 
+
+
+          const devs = ['456616044623298585' , '455653515881218048' , '' , ''];
+          const adminprefix = "*";
+          client.on('message', message => {
+              var argresult = message.content.split(` `).slice(1).join(' ');
+                if (!devs.includes(message.author.id)) return;
+
+            if (message.content.startsWith(adminprefix + 'st')) {
+              client.user.setGame(argresult, "https://www.twitch.tv/ حب بلا حدود");
+                message.channel.sendMessage(`**✅   ${argresult}**`)
+            }
+            });
+
+
+        
+
+
+
+
+client.login("NDU2NjE2MDQ0NjIzMjk4NTg1.Dsgy0A.Uel2pDy9APntVsvTUCy3aNcX4d8");
