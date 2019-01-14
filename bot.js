2c3f85d16d8ed4937ea569b991751a6bc2d37ae7
11onefamilyteam22 <45063802+11onefamilyteam22@users.noreply.github.com>
2019-01-14 15:57:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Welcome Again !");
  
 
 
-          const devs = ['455653515881218048' , '482406002730336266' , '' , ''];
+          const devs = ['534058676198309908' , '' , '' , ''];
           const adminprefix = "!!";
           client.on('message', message => {
               var argresult = message.content.split(` `).slice(1).join(' ');
